a01b007b9e639b47ec77faa31b1bb80b2467f86c
DESKTOP-UFNQ98L\likeit_student <ryota.hisaura39@gmail.com>
2017-08-17 18:44:59 +0900

    PTra09_02まで完了

--- Practice/src/practice09/PTra09_01.java
+++ Practice/src/practice09/PTra09_01.java
@@ -9,11 +9,13 @@ import practice09.util.Util;
  */
 
 public class PTra09_01 {
+
 	public static void main(String[] args) {
 		String str = "この文字列をゆっくり表示したい";
-		
+
 		// ★ 変数strを、Util内のメソッドを用いて0.5秒間隔で表示させなさい
 		// ※ Utilはインポートしても良いし、完全限定名で記述しても良いです
-		System.out.println(Util.dispMessageSlowly(str,500));
+		Util.dispMessageSlowly(str,500);
 	}
+
 }
--- Practice/src/practice09/PTra09_02.java
+++ Practice/src/practice09/PTra09_02.java
@@ -1,10 +1,6 @@
 package practice09;
-/*
- * PTra09_02.java
- *   作成	LIKEIT	2017
- *------------------------------------------------------------
- * Copyright(c) Rhizome Inc. All Rights Reserved.
- */
+
+import practice09.util.Util;
 
 public class PTra09_02 {
 	public static void main(String[] args) {
@@ -12,9 +8,13 @@ public class PTra09_02 {
 
 		// ★ 配列arrayを、Util内のメソッドを用いてランダムに並び替えてください
 		// ※ Utilはインポートしても良いし、完全限定名で記述しても良いです
-
+		String[] list =Util.shuffleArray(array);
 
 		// ★ ランダムに並び替えた後の配列を前から順に全て表示してください
+		for(String value:list) {
 
+			System.out.println(value);
+
+			}
 	}
 }
