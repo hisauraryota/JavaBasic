7402b16cb34eadbf884405b915f30361f3bfca76
DESKTOP-UFNQ98L\likeit_student <ryota.hisaura39@gmail.com>
2017-09-14 14:47:03 +0900

    PTra16_03まで完了

--- Practice/src/practice16/PTra16_01.java
+++ Practice/src/practice16/PTra16_01.java
@@ -1,13 +1,23 @@
 package practice16;
 
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 /*
  */
 public class PTra16_01 {
-    public static void main(String[] args) {
+    public static void main(String[] args)  {
 
         /*
         * ★ java.util.Dateクラスを使って、自分の生年月日を「YYYY年MM月DD日」形式で出力してください
         *  ※　他の標準クラスも使って良いです
         */
+    	Date birthday = new Date(96,2,9);
+
+    	SimpleDateFormat f = new SimpleDateFormat("YYYY年MM月dd日");
+    	System.out.println(f.format(birthday));
+
+
+
     }
 }
--- Practice/src/practice16/PTra16_02.java
+++ Practice/src/practice16/PTra16_02.java
@@ -19,5 +19,25 @@ public class PTra16_02 {
 		double d = 453.592;
 
 		// ★ 上記基本型を、それぞれのラッパークラスに代入してください
+
+		Boolean B = new Boolean(b);
+		Byte By = new Byte(by);
+		Short S = new Short(s);
+		Character C = new Character(c);
+		Integer I = new Integer(i);
+		Long L = new Long(l);
+		Float F = new Float(f);
+		Double D = new Double(d);
+
+		System.out.println(B);
+		System.out.println(By);
+		System.out.println(S);
+		System.out.println(C);
+		System.out.println(I);
+		System.out.println(L);
+		System.out.println(F);
+		System.out.println(D);
+
+
 	}
 }
