fae414bbab4f479eb48f9f7b26e885e62713fa32
DESKTOP-UFNQ98L\likeit_student <ryota.hisaura39@gmail.com>
2017-09-14 18:57:57 +0900

    PTra18_01まで完了

---
+++ Practice/src/practice18/Player.java
@@ -0,0 +1,42 @@
+package practice18;
+
+public class Player{
+	
+	private String position;
+	private String name;
+	private String country;
+	private String team;
+	
+	public String getposition() {
+		return position;
+	}
+	public void setnposition(String position) {
+		 this.position = position;
+	 }
+	 
+	public String getname() {
+		return name;
+	}
+	public void setname(String name) {
+		 this.name = name;
+	 }
+	
+	public String getcountry() {
+		return country;
+	}
+	public void setcountry(String country) {
+		 this.country = country;
+	 }
+	
+	public String getteam() {
+		return team;
+	}
+	public void setteam(String team) {
+		 this.team = team;
+	 }
+
+	public String toString() {
+		return position + "," + name + "," + country + "," +team;
+	}
+
+}
